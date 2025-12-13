e59b46ac65025ce33f2559dc9ea4fa18deb3b4a6
Badri Gaire <badrigaire212@gmail.com>
2025-12-13 16:48:10 +0545

    add toggle buttons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.badrigaire.com.np',
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
   vite: {
     plugins: [tailwindcss()]
   }
