a41ad147eb6cf5344493bb325ebe000071dbc0a3
Badri Gaire <badrigaire212@gmail.com>
2026-02-23 22:53:58 +0545

    Changes to add lab sub-domain

--- src/utils/siteConfig.ts
+++ src/utils/siteConfig.ts
@@ -4,10 +4,13 @@ export const siteConfig = {
     "Badri Gaire is a professional Software, Web, and Frontend Developer based in Bhairahawa, Rupandehi, Nepal. Specializing in high-performance website development, SEO, and modern web applications at Bhairahawa.",
   lang: "en-US",
   author: "Badri",
+  email: "badrigaire212@gmail.com",
   site: "https://www.badrigaire.com.np",
+  lab: "https://lab.badrigaire.com.np",
   socialLinks: [
     "https://www.linkedin.com/in/badri-gaire-85881a334/",
     "https://github.com/Badri-Gaire",
+    "https://lab.badrigaire.com.np",
   ],
   gaId: "G-QJ0ZE2JK62",
   twitterHandle: "@BadriGaire", // Replace with your actual handle if different
@@ -33,9 +36,5 @@ export const menuLinks = [
   {
     title: "Blog",
     path: "/blog",
-  },
-  {
-    title: "Contact",
-    path: "/contact",
   },
 ];
